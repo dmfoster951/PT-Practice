4a08a112fffc7c02a94dc480897393c3ee152235
Daniel Foster <91345376+dmfoster951@users.noreply.github.com>
2023-10-23 12:27:18 -0400

    Add files via upload
    Medium problem - pretty simple - strings, splitting, nested loops.

---
+++ LM 2016/MedSecret.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class MedSecret {
+    public static void main(String [] args) {
+        Scanner scan = new Scanner(System.in);
+        int cases = Integer.parseInt(scan.nextLine());
+        while (cases-- > 0) {
+            // Read message.
+            int m = Integer.parseInt(scan.nextLine());
+            String [] lines = new String[m];
+            for (int i = 0; i < m; i++) lines[i] = scan.nextLine();
+
+            // Starting row, column.
+            String [] yx = scan.nextLine().split(",");
+            int row = Integer.parseInt(yx[0]);
+            int col = Integer.parseInt(yx[1]);
+
+            // Read cover.
+            int co = Integer.parseInt(scan.nextLine());
+            String [] cov = new String[co];
+            for (int i = 0; i < co; i++) cov[i] = scan.nextLine();
+
+            // Find secret.
+            String secret = "";
+            for (int r = 0; r < cov.length; r++) {
+                // While there are still Os in the line.
+                while (cov[r].indexOf('O') != -1) {
+                    // 
+                    secret += lines[row + r].charAt(col + cov[r].indexOf('O'));
+                    cov[r] = cov[r].replaceFirst("O", "-");
+                }
+            }
+            System.out.println(secret);
+        }
+        scan.close();
+    }
+}
