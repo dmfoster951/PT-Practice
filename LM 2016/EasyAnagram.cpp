69bc324671586c789a0bdc039254eb4103327b8c
Daniel Foster <91345376+dmfoster951@users.noreply.github.com>
2023-10-23 21:59:16 -0400

    Add files via upload
    Easy - very simple once you know to sort them first - strings, sorting.

---
+++ LM 2016/EasyAnagram.cpp
@@ -0,0 +1,48 @@
+using namespace std;
+#include <bits/stdc++.h>
+
+// Split string into string vector.
+vector<string> split(string line, string delim) {
+    vector<string> res;
+    // Find next token, add to vector, and pull it out of the string.
+    while (line.find(delim) != string::npos) {
+        int delimPoint = line.find(delim);
+        res.push_back(line.substr(0, delimPoint));
+        line = line.substr(delimPoint + delim.size());
+    }
+    res.push_back(line);
+    return res;
+}
+
+// Return string input of a line.
+string strLine() {
+    string t;
+    getline(cin, t);
+    return t;
+}
+
+// Get int input of a line with 1 number.
+int intLine() {
+    return stoi(strLine());
+}
+
+int main() {
+    // Test cases.
+    int c = intLine();
+    while (c-- > 0) {
+        // Split input into words.
+        string line = strLine();
+        cout << line << " = ";
+
+        vector<string> words = split(line, "|");
+        if (words[0] == words[1]) {
+            // If already the same, can't be anagram.
+            cout << "NOT AN ANAGRAM" << endl;
+        } else {
+            // Sort both by alpha. If equal when sorted, then anagram.
+            for (auto &s : words) sort(s.begin(), s.end());
+            if (words[0] == words[1]) cout << "ANAGRAM" << endl;
+            else cout << "NOT AN ANAGRAM" << endl;
+        }
+    }
+}
