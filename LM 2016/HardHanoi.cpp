b6931e64f5d010b8b1f87905b50ee7a3d8288370
Daniel Foster <91345376+dmfoster951@users.noreply.github.com>
2023-10-23 20:35:51 -0400

    Add files via upload
    Hard - couldn't do if not already familiar with algorithm.

---
+++ LM 2016/HardHanoi.cpp
@@ -0,0 +1,41 @@
+using namespace std;
+#include <bits/stdc++.h>
+
+// Return string input of a line.
+string strline() {
+    string t;
+    getline(cin, t);
+    return t;
+}
+
+// Get int input of a line with 1 number.
+int intline() {
+    return stoi(strline());
+}
+
+// Recursive solution.
+// Would not get if not already familiar with algorithm.
+// hanoi(3, 'A', 'C', 'B')
+void hanoi(int n, char start, char end, char help) {
+    if (n == 0) return;
+
+    // Move everything but bottom to B.
+    hanoi(n - 1, start, help, end);
+
+    // Move bottom to C.
+    cout << start << "->" << end << endl;
+
+    // Move everthing to C on top.
+    hanoi(n - 1, help, end, start);
+}
+
+int main() {
+    // Test cases.
+    int cases = intline();
+    while (cases-- > 0) {
+        // Number of disks.
+        int n = intline();
+        cout << n << endl;
+        hanoi(n, 'A', 'C', 'B');
+    }
+}
